49ef7fa3426a71cd7528841b23aec8e66b136745
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-10-29 20:49:23 +0300

    Task-2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задание_1");
         for (byte i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("\n" + "Задание_2");
+        for (byte i = 10; i > 0; i--) {
             System.out.println(i);
         }
     }
